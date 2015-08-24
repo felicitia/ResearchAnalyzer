1028a6c825995f39e37b198db8b131c94e672589
Yixue Zhao <felicitia2010@gmail.com>
2015-08-24 11:14:51 -0700

    finish the analyzer
    add arch_smell counter

--- src/leo/ConcernCounter.java
+++ src/leo/ConcernCounter.java
@@ -23,18 +23,24 @@ public class ConcernCounter {
 			+ "archtecturalProblemsMM.csv";
 	private static final String concernCsvFile = rootPath + "agglomeration.csv";
 	private static final String outputFolder = "/Users/felicitia/Google_Drive/Design_Code/yixue_results/MobileMedia/";
-	private static final String codeSmellOutput = outputFolder+"code_smell.csv";
-	private static final String COMMA_SEPARATOR = ",";
-	private static final String SEMI_SEPARATOR = ";";
-	
+	private static final String codeSmellOutput = outputFolder
+			+ "code_smell.csv";
+	private static final String archSmellOutput = outputFolder
+			+ "arch_smell.csv";
+	private static final String COMMA = ",";
+	private static final String SEMI = ";";
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		countCodeSmell(codeSmellOutput);
+		// countCodeSmell(codeSmellOutput);
+		countArchSmell(archSmellOutput);
 	}
 
-	public static void countArchSmell(final String outputFile){
+	public static void countArchSmell(final String outputFile) {
 		JSONObject concerns = ConcernAnalyzer.getJsonFromCsv(concernCsvFile);
-		String[] header = {"concern#", "#classes", "#related", "#not_related", "related_classes", "not_related_classes"};
+		HashMap<String, int[]> classSmellResult = (HashMap<String, int[]>) getArchSmells();
+		String[] header = { "concern#", "#classes", "#arch_smell", "#related",
+				"#not_related", "related_classes", "not_related_classes" };
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter(outputFile);
@@ -42,79 +48,119 @@ public class ConcernCounter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		setCsvHeader(writer, ";", header);
-		
-	}
-	
-	public static void setCsvHeader(PrintWriter writer, String SEPARATOR, final String[] header){
-		if(!SEPARATOR.equals(",")){
-			writer.println("sep="+SEPARATOR);
-		}
-		for(int i=0; i<header.length-1; i++){
-			writer.print(header[i]+SEPARATOR);
-		}
-		writer.println(header[header.length-1]);
-	}
-	
-	public static void countCodeSmell(final String outputFile) {
-		HashMap<String, HashSet<String>> classSmellResult = (HashMap<String, HashSet<String>>) getCodeSmells();
-		String[] header = {"concern#", "#code_smells"};
-		JSONObject concerns = ConcernAnalyzer.getJsonFromCsv(concernCsvFile);
-		PrintWriter writer = null;
-		try {
-			writer = new PrintWriter(outputFile);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-//		writer.println("concern#"+COMMA_SEPARATOR+"#code_smells");
-		setCsvHeader(writer, ",", header);
+		setCsvHeader(writer, SEMI, header);
 		for (Iterator iterator = concerns.keySet().iterator(); iterator
 				.hasNext();) {
 			String key = (String) iterator.next();
 			JSONObject concern = (JSONObject) concerns.get(key);
 			JSONArray classes = (JSONArray) concern.get("classes");
-			int num=0;
+			int numPerConcern = 0;
+			JSONArray relatedClass = new JSONArray();
+			JSONArray notRelatedClass = new JSONArray();
 			for (int i = 0; i < classes.size(); i++) {
 				String className = classes.get(i).toString().trim();
-				if(classSmellResult.containsKey(className)){
+				int numPerClass = 0;
+				if (classSmellResult.containsKey(className)) {
+					int[] smellArray = classSmellResult.get(className);
+					for (int j = 0; j < smellArray.length; j++) {
+						numPerClass += smellArray[j];
+					}
+					if (0 == numPerClass) {
+						notRelatedClass.add(className);
+					} else {
+						relatedClass.add(className);
+					}
+				} else {
+					notRelatedClass.add(className);
+				}
+				numPerConcern += numPerClass;
+			}
+			writer.println(key + SEMI + classes.size() + SEMI + numPerConcern + SEMI
+					+ relatedClass.size() + SEMI + notRelatedClass.size()
+					+ SEMI + relatedClass.toJSONString() + SEMI
+					+ notRelatedClass.toJSONString());
+		}
+		writer.close();
+		System.out.println("architectural smell done! (◍′◡‵◍)");
+	}
+
+	public static void setCsvHeader(PrintWriter writer, String SEPARATOR,
+			final String[] header) {
+		if (!SEPARATOR.equals(COMMA)) {
+			writer.println("sep=" + SEPARATOR);
+		}
+		for (int i = 0; i < header.length - 1; i++) {
+			writer.print(header[i] + SEPARATOR);
+		}
+		writer.println(header[header.length - 1]);
+	}
+
+	public static void countCodeSmell(final String outputFile) {
+		HashMap<String, HashSet<String>> classSmellResult = (HashMap<String, HashSet<String>>) getCodeSmells();
+		String[] header = { "concern#", "#code_smells" };
+		JSONObject concerns = ConcernAnalyzer.getJsonFromCsv(concernCsvFile);
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(outputFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setCsvHeader(writer, COMMA, header);
+		for (Iterator iterator = concerns.keySet().iterator(); iterator
+				.hasNext();) {
+			String key = (String) iterator.next();
+			JSONObject concern = (JSONObject) concerns.get(key);
+			JSONArray classes = (JSONArray) concern.get("classes");
+			int num = 0;
+			for (int i = 0; i < classes.size(); i++) {
+				String className = classes.get(i).toString().trim();
+				if (classSmellResult.containsKey(className)) {
 					num += classSmellResult.get(className).size();
 				}
 			}
-			writer.println(key+COMMA_SEPARATOR+num);
+			writer.println(key + COMMA + num);
 		}
 		writer.close();
 		System.out.println("code smell done! ╭( ･ㅂ･)و ");
 	}
 
 	/**
-	 * HashMap<String, Set<String>> is the result of arch smell set for each class
+	 * HashMap<String, Set<String>> is the result of arch smell set for each
+	 * class
+	 * 
 	 * @return
 	 */
 	public static Map getArchSmells() {
-		Map<String, HashSet<String>> classSmellResult = new HashMap<String, HashSet<String>>();
+		Map<String, int[]> classSmellResult = new HashMap<String, int[]>();
 		BufferedReader br = null;
 		String line = "";
-		String cvsSplitBy = ";";
+		String cvsSplitBy = SEMI;
 		try {
 			br = new BufferedReader(new FileReader(archSmellFile));
 			// skip sep=; (first line)
 			br.readLine();
-			//get archSmellList
+			// get archSmellList
 			line = br.readLine();
 			String[] tmpLine = line.split(cvsSplitBy);
-			String[] archSmellList = Arrays.copyOfRange(tmpLine, 2, tmpLine.length);
+			String[] archSmellList = Arrays.copyOfRange(tmpLine, 2,
+					tmpLine.length);
 			while ((line = br.readLine()) != null) {
-				// use comma as separator
 				String[] lineArray = line.split(cvsSplitBy);
-				String elementName = lineArray[0];
+				String elementName = lineArray[1];
 				String className = getClassName(elementName);
 				if (classSmellResult.containsKey(className)) {
-					HashSet<String> tmpSet = classSmellResult.get(className);
-					tmpSet.addAll(getSmellSet(lineArray));
-					classSmellResult.put(className, tmpSet);
+					int[] tmpArray = classSmellResult.get(className);
+					int[] tmpArray2 = getArchSmellArray(lineArray,
+							archSmellList);
+					// sum two tmp arrays to tmpArray
+					for (int i = 0; i < archSmellList.length; i++) {
+						tmpArray[i] += tmpArray2[i];
+					}
+					classSmellResult.put(className, tmpArray);
 				} else {
-					classSmellResult.put(className, getSmellSet(lineArray));
+					classSmellResult.put(className,
+							getArchSmellArray(lineArray, archSmellList));
 				}
 			}
 		} catch (FileNotFoundException e) {
@@ -132,16 +178,18 @@ public class ConcernCounter {
 		}
 		return classSmellResult;
 	}
-	
+
 	/**
-	 * HashMap<String, HashSet<String>> is the result of code smell set for each class
+	 * HashMap<String, HashSet<String>> is the result of code smell set for each
+	 * class
+	 * 
 	 * @return
 	 */
 	public static Map getCodeSmells() {
 		Map<String, HashSet<String>> classSmellResult = new HashMap<String, HashSet<String>>();
 		BufferedReader br = null;
 		String line = "";
-		String cvsSplitBy = ";";
+		String cvsSplitBy = SEMI;
 		try {
 			br = new BufferedReader(new FileReader(codeSmellFile));
 			// skip headers (first line)
@@ -153,10 +201,10 @@ public class ConcernCounter {
 				String className = getClassName(elementName);
 				if (classSmellResult.containsKey(className)) {
 					HashSet<String> tmp = classSmellResult.get(className);
-					tmp.addAll(getSmellSet(lineArray));
+					tmp.addAll(getCodeSmellSet(lineArray));
 					classSmellResult.put(className, tmp);
 				} else {
-					classSmellResult.put(className, getSmellSet(lineArray));
+					classSmellResult.put(className, getCodeSmellSet(lineArray));
 				}
 			}
 		} catch (FileNotFoundException e) {
@@ -175,7 +223,23 @@ public class ConcernCounter {
 		return classSmellResult;
 	}
 
-	public static HashSet<String> getSmellSet(String[] lineArray) {
+	public static int[] getArchSmellArray(String[] lineArray,
+			String[] archSmellList) {
+		// process the code smells, to avoid redundancy
+		int[] smellNum = new int[archSmellList.length];
+		for (int i = 2; i < lineArray.length; i++) {
+			int num;
+			try {
+				num = Integer.parseInt(lineArray[i]);
+			} catch (NumberFormatException e) {
+				num = 0;
+			}
+			smellNum[i - 2] = num;
+		}
+		return smellNum;
+	}
+
+	public static HashSet<String> getCodeSmellSet(String[] lineArray) {
 		// process the code smells, to avoid redundancy
 		HashSet<String> smellSet = new HashSet<String>();
 		for (int i = 1; i < lineArray.length; i++) {
@@ -194,15 +258,24 @@ public class ConcernCounter {
 		return smellSet;
 	}
 
+	/**
+	 * check elementName to see whether it's class or method also remove
+	 * duplicate class name (which is weird... actually shouldn't deal with it
+	 * here.. it's a bug in the input file, need to resolve later
+	 * 
+	 * @param elementName
+	 * @return class name
+	 */
 	public static String getClassName(String elementName) {
 		String[] splitNames = elementName.split("\\.");
-		//find duplicate name (to deal with the bug of input file)
+		// find duplicate name (to deal with the bug of input file)
 		Set<String> uniqueNames = new HashSet<String>(Arrays.asList(splitNames));
-		if(splitNames.length!=uniqueNames.size()){
-			System.out.println("duplicate name (original) = "+elementName);
-			if(splitNames[splitNames.length-1].equals(splitNames[splitNames.length-2])){
-				//remove last element in spiltNames
-				splitNames = Arrays.copyOf(splitNames, splitNames.length-1);
+		if (splitNames.length != uniqueNames.size()) {
+			System.out.println("duplicate name (original) = " + elementName);
+			if (splitNames[splitNames.length - 1]
+					.equals(splitNames[splitNames.length - 2])) {
+				// remove last element in spiltNames
+				splitNames = Arrays.copyOf(splitNames, splitNames.length - 1);
 			}
 		}
 		char firstChar = splitNames[splitNames.length - 1].charAt(0);
